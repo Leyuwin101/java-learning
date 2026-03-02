63e4fbce2215c842ad3a8b9eebf9f4f9752a92b1
leeuwindave732-lang <leeuwindave732@gmail.com>
2026-03-02 21:40:25 +0800

    mini

---
+++ mini/mini11.java
@@ -0,0 +1,52 @@
+package mini;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class mini11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean calculate = true;
+        while (calculate) {
+            System.out.println("\n===== WELCOME TO CALCULATOR ======");
+            System.out.print(">>> Enter the first number: ");
+            double num1 = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.print(">>> Enter an operator(+, -, x, /): ");
+            String ope = sc.nextLine();
+
+            System.out.print(">>> Enter the second number: ");
+            double num2 = sc.nextDouble();
+            sc.nextLine();
+
+            try {
+                double choice = switch (ope) {
+                    case "+" -> num1 + num2;
+                    case "-" -> num1 - num2;
+                    case "x" -> num1 * num2;
+                    case "/" -> num2 == 0 ? 0 : num1 / num2;
+                    default -> throw new IllegalArgumentException("Invalid operation " + ope);
+                };
+
+                System.out.println("The result is: " + choice );
+
+            } catch (InputMismatchException e) {
+                System.out.println("Input is not a number");
+            }
+
+            System.out.println("Do you want to calculate again? (yes/no)");
+            String again = sc.nextLine();
+
+            if (again.equalsIgnoreCase("no")) {
+                calculate = false;
+            }
+
+
+        }
+
+        sc.close();
+
+    }
+}
