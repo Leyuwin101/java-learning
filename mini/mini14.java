314ef04690f1198131d522bd8d2294496483262f
leeuwindave732-lang <leeuwindave732@gmail.com>
2026-03-22 17:44:46 +0800

    mini

--- mini/mini14.java
+++ mini/mini14.java
@@ -1,5 +1,34 @@
 package mini;
 
 public class mini14 {
-    
+    public static void main(String[] args) {
+        
+        Thread even = new Thread(() -> { 
+            for ( int i = 2; i <= 20; i += 2) {
+                System.out.println("Even: " + i);
+
+                try {
+                    Thread.sleep(500);
+                } catch(InterruptedException e ) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        Thread odd = new Thread(() -> {
+            for ( int i = 1; i <= 19; i += 2) {
+                System.out.println("Odd: " + i);
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        odd.start();
+        even.start();
+    }
 }
